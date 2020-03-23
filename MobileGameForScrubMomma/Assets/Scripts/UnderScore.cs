ace72d6c32076277a3aac05c710b000eea2b89dc
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-03-23 13:28:31 +1100

    fe

---
+++ MobileGameForScrubMomma/Assets/Scripts/UnderScore.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderScore : MonoBehaviour
+{
+    public bool hit, miss;
+    public int BoyFriend_Kissing_kiss_count = 0;
+    public bool miss_chance = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Boyfriend_task()
+    {
+        FindGirl();
+    }
+    void FindGirl()
+    {
+        if (miss == false && hit == false)
+        {
+            
+            miss = true;
+            hit = false;
+        }
+        else if (hit == false && miss == true)
+        {
+            
+            miss = false;
+            hit = true;
+            BoyFriend_Kissing_kiss_count += 1;
+            Debug.Log("That_dab");
+        }
+        else if (hit == true && miss == false)
+        {
+            
+            miss = true;
+            hit = true;
+        }
+        else
+        {
+            
+            miss = false;
+            hit = false;
+            Debug.Log(BoyFriend_Kissing_kiss_count);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Boyfriend_task();
+
+
+
+    }
+}
